69b7c8ad44be5c749adca86c8a98fdc52aeea075
Sierisimo Sier <sierisimo@gmail.com>
2021-11-11 23:48:45 -0600

    Adding build rules for compose

--- example-app/build.gradle.kts
+++ example-app/build.gradle.kts
@@ -41,6 +41,13 @@ android {
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_11.toString()
     }
+
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.1.0-beta02"
+    }
 }
 
 dependencies {

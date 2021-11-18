d47e20f7444be20d093a94100b26e7d674f6c076
Sebastian Tellez <sebastian.tellez.o@gmail.com>
2021-11-17 23:16:29 -0600

    refactor and create html

--- k-pop/src/main/kotlin/com/kotlinerskt/kpop/DocGenerator.kt
+++ k-pop/src/main/kotlin/com/kotlinerskt/kpop/DocGenerator.kt
@@ -1,15 +1,34 @@
 package com.kotlinerskt.kpop
 
-import com.google.devtools.ksp.symbol.KSFile
-import kotlinx.html.dom.document
+import com.google.devtools.ksp.symbol.FileLocation
+import kotlinx.html.*
+import kotlinx.html.stream.appendHTML
 
 
-typealias NonCareFiles = List<Pair<KSFile, KSNodesList>>
-typealias OffenderFiles = List<Pair<KSFile, KSNodesList>>
+fun createHtmlDoc(partitionedFiles: List<FileOffender>): String {
 
-fun createDoc(partitionedFiles: Pair<NonCareFiles, OffenderFiles>) {
+    return buildString {
+        appendHTML().html {
+            body {
+                h1 {
+                    +"Offender files"
+                }
 
-    val documentFile = document {
-
+                ul {
+                    partitionedFiles.forEach { offenderFile ->
+                        val file = offenderFile.fileDeclaration
+                        val offendersMethods = offenderFile.offenderClasses.first().offenderMethods
+                        offendersMethods.forEach { method ->
+                            li {
+                                +"File ${file.fileName}"
+                                br
+                                val methodLocation = (method.location as FileLocation)
+                                +"Method ${method.simpleName.asString()}: ${methodLocation.filePath}:${methodLocation.lineNumber}"
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
